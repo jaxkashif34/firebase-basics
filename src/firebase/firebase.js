ca53781e54f1700d8aa7c1a776a478556c62f1df
Kashif Ali <jaxksahif34@gmail.com>
2022-08-30 11:23:11 +0500

    initial commit

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,17 +1,17 @@
-import { initializeApp } from "firebase/app"
-import { getFirestore } from "firebase/firestore"
-import { getDatabase } from "firebase/database"
-import { getStorage } from "firebase/storage"
+import { initializeApp, getApp, getApps } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getDatabase } from 'firebase/database';
+import { getStorage } from 'firebase/storage';
 const firebaseConfig = {
-    apiKey: "AIzaSyAf96e0uxNhM9xkRT_tsR77EsUFxemvFnE",
-    authDomain: "fir-frontend-88140.firebaseapp.com",
-    projectId: "fir-frontend-88140",
-    storageBucket: "fir-frontend-88140.appspot.com",
-    messagingSenderId: "28849543251",
-    appId: "1:28849543251:web:0c1c5c5d48587a4f230ea6",
-    measurementId: "G-GLP8RL9E96"
+  apiKey: 'AIzaSyAf96e0uxNhM9xkRT_tsR77EsUFxemvFnE',
+  authDomain: 'fir-frontend-88140.firebaseapp.com',
+  projectId: 'fir-frontend-88140',
+  storageBucket: 'fir-frontend-88140.appspot.com',
+  messagingSenderId: '28849543251',
+  appId: '1:28849543251:web:0c1c5c5d48587a4f230ea6',
+  measurementId: 'G-GLP8RL9E96',
 };
-export const app = initializeApp(firebaseConfig);
+export const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
 export const cf = getFirestore(app);
-export const db = getDatabase(app)
-export const st = getStorage(app)
+export const db = getDatabase(app);
+export const st = getStorage(app);
